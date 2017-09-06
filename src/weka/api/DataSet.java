e920f4b920b518898fa335819f7079b27af8f69d
cmrudi <cutmeurahrudi@gmail.com>
2017-09-06 21:34:09 +0700

    update gitignore to ignore filePath

--- src/weka/api/DataSet.java
+++ src/weka/api/DataSet.java
@@ -1,5 +1,19 @@
 package weka.api;
 
+import java.util.List;
+
 public class DataSet {
+	List<Message> messageList;
+	String filePath;
+	
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+	
+	public void extractFile() {
+		if (filePath != null) {
+			
+		}
+	}
 
 }
